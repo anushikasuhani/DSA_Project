58b20f765255f4bc3f098acd90c6eec5bae4bf31
sanduni nilwala perera <sandunilwala1991@gmail.com>
2015-03-08 09:06:58 +0530

    Update HomeFile Class

--- HomeFile.java
+++ HomeFile.java
@@ -7,8 +7,8 @@ package myclass;//change the package name
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.FileWriter;
-import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 
 
 
@@ -16,43 +16,31 @@ public class HouseFile
 {
 		
 	public File file;
-        boolean bool = false;
+	boolean bool = false;
 
 	//Class Constructor to create text file
 	public HouseFile() throws IOException
 	{
 	
 		 this.file = new File("housefile.txt");// create new text files
-		 if(!file.exists())
-		 {
- 			bool = file.createNewFile();
-			//create new text file if it does not exist
- 		 }
-         
-
-		
 	}
 	
-	/*public static void main(String[] args) throws IOException 
+
+	public boolean saveHouseDataFile(Object data) throws IOException
 	{
-	//testing main function	
-		
-		HouseFile h = new HouseFile();
-		//h.saveHouseDataFile("ABC");
-		h.saveHouseDataFile("");
-	}*/
-	public boolean saveHouseDataFile(String data)
-	{
+		FileOutputStream tfileOut;//create fileoutputstream
 		
 		
 		try 
 		{
 	       
-    		FileWriter fdata = new FileWriter(file.getName(),true);
-    	        BufferedWriter bufferWritter = new BufferedWriter(fdata);
-    	        bufferWritter.write(data);//add new record to the text file
-    	        bufferWritter.write("\n");//add new line after each record
-    	        bufferWritter.close();
+    		tfileOut = new FileOutputStream(file.getAbsolutePath(),true);
+			 ObjectOutputStream tobjout = new ObjectOutputStream(tfileOut);
+			 tobjout.writeObject(data);
+			 tobjout.writeObject("\n");
+			 tobjout.close();
+			 tfileOut.close();
+		
  
 	        
 			
